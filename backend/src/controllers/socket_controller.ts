263c611a4416839c9956a6ecb0fcf0aa855ddb50
SofiaPasco <sofia_kit@hotmail.com>
2024-02-27 12:20:59 +0100

    added types for jointhegame

--- backend/src/controllers/socket_controller.ts
+++ backend/src/controllers/socket_controller.ts
@@ -1,12 +1,18 @@
 /**
  * Socket Controller
  */
+/**
+ * Socket Controller
+ */
 import Debug from "debug";
 import { Server, Socket } from "socket.io";
 import { ClientToServerEvents, ServerToClientEvents } from "@shared/types/SocketTypes";
 
 // Create a new debug instance
 const debug = Debug("backend:socket_controller");
+
+// Skapa en array för att spåra väntande spelare
+let waitingPlayers =[];
 
 // Handle a user connecting
 export const handleConnection = (
